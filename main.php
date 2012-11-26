cc324ca2fe2d1c16cbb8ad596575e3a2f653e35c
jon <test@test.com>
2012-11-26 22:29:54 +0800

    依照自动抓取的目录结果_result.log自动抓取，不再需要粘贴cookieURL和indexURL

--- main.php
+++ main.php
@@ -11,42 +11,39 @@ require_once "lib/function.php";
 makeDir("./data");
 makeDir("./html");
 
-$fpClass = fopen("./class.log", "r");
-$fpIndexURL = fopen("./indexURL.log", "r");
-$fpCookieURL = fopen("./cookieURL.log", "r");
-
-if(!$fpClass || !$fpIndexURL || !$fpCookieURL)
+$class = $argv[1];
+if(!$class)
 {
-	echo "fopen() error \n";
+	echo "you have to specify a directory: 'A', 'B', 'C' ...\n";
 	exit;
 }
 
-while($class=getClass($fpClass))
+$indexFileName = "./index/$class/_result.log";
+if(!file_exists($indexFileName))
 {
-	//$class = getClass($fpClass);              //命令行获取学科分类
-	$indexURL = getIndexURL($fpIndexURL);      //首页地址
-	$cookieURL = getCookieURL($fpCookieURL);    //初始化cookie，防止被识破
+	echo "file $indexFileName not exists \n";
+	exit;
+}
+
+$fp = fopen($indexFileName, "r");
+
+while($line=readLine($fp))
+{
+	$className = getClassName($line);
+	$code = getClassCode($line);
+	$indexURL = getIndexURL($code);       //首页地址
+	$cookieURL = getCookieURL($code);    //初始化cookie，防止被识破
 
 	$indexURL = cleanIndexURL($indexURL);
 
-	if(!$class || !indexURL || !cookieURL)
+	if(!$className || !$indexURL || !$cookieURL)
 	{
 		echo "Usage: \$php main.php";
 		exit;
 	}
 
-	main($class, $cookieURL, $indexURL);
+	main($className, $cookieURL, $indexURL);
 	
 	echo "**********************抓取$class 成功*************************\n";
 	
 }//end while
-
-/*
-$url = "./html/基础科学-自然科学研究/1.html";
-$url = iconv("UTF-8","gb2312",$url);
-$content = file_get_contents($url);
-
-$match = parseOrigin($content);
-
-var_dump($match);
-*/
